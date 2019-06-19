b2191b0e76891da79d244dd1c464dadb506aa8b4
Kevin Pham <pham78kevin@yahoo.com>
2019-06-19 16:47:19 -0700

    Add files via upload

---
+++ Playground.py
@@ -0,0 +1,20 @@
+
+print("Hello World")
+
+message = "Hello World"
+
+print(message) 
+
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+   return
+
+printme("Hello")
+
+def main(): 
+	print("I have defined main here")
+	return
+
+main()
+
